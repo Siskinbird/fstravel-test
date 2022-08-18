92d5bb0e80f5bde57b130147b740d96068b07fef
Dmitrii Chizhov <dimachig@gmail.com>
2022-08-18 10:52:26 +0300

    refactor: async function in content

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -11,7 +11,7 @@
 
 import Author from '@/components/Author.vue'
 import CardOne from '@/components/CardOne.vue'
-import data from '@/assets/jsonData.json'
+//import data from '@/assets/jsonData.json'
 
 export default {
   name: 'Content',
@@ -21,10 +21,28 @@ export default {
   },
   data() {
     return {
-      seminars: data.seminars.filter(el => el.type === 'seminar'),
-      training: data.seminars.filter(el => el.type === 'training')
+      dataUrl: 'https://eg-cdn.s3.eu-central-1.amazonaws.com/static/fe-test/seminars-test-data.json',
+      seminars: [],
+      training: [],
+      errors: []
+      //seminars: data.seminars.filter(el => el.type === 'seminar'),
+      //training: data.seminars.filter(el => el.type === 'training')
     }
-  }
+  },
+  async mounted() {
+    try {
+      const response = await fetch(this.dataUrl)
+      const result = await response.json()
+      console.log(result)
+      this.seminars.push(...result.seminars.filter(el => el.type === 'seminar'))
+      this.training.push(...result.seminars.filter(el => el.type === 'training'))
+      console.log(this.seminars)
+      console.log(this.training)
+    }
+    catch(error) {
+    this.errors.push(error)
+  } 
+  } 
 }
 </script>
 
